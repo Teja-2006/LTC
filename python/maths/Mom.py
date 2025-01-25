7de0458aa589f164ae5cc6fc654717b2b1a8517f
Teja-2006 <tejascs289022@gmail.com>
2025-01-25 11:19:10 +0530

    Multiplicaiton of a Matrix

---
+++ python/maths/Mom.py
@@ -0,0 +1,15 @@
+def mult(r1,c1,r2,c2):
+    for i in r1:
+        for j in c1:
+            m1[i][j]
+            
+    return result
+m1[100][100], m2[100][100],result [100][100]
+[r1,c1,r2,c2] = map(int,input("Enter the size rows and column of the frist second matrix respectively: ").split(" "))
+if c1==r2:
+    mult(r1,c1,r2,c2)
+else:
+    print("Please,make sure m1 no.of rows = m2 no.of columns :D")
+    
+    
+    
